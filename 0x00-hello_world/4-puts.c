620316df9aa404ba4170d7f41cd8d8d2b7aa7360
Lennox Mountain <lennoxmountain67@gmail.com>
2023-02-16 19:00:25 -0800

    crazy betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-
+/**
+ *main - this program prints using the puts function
+ *Return: 0 (success)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
