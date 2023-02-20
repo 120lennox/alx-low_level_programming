a2a8b2b80eca731eefd1f5419ca922c9060a841b
Lennox Mountain <lennoxmountain67@gmail.com>
2023-02-20 14:52:28 -0800

    genius minds

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,9 @@
  */
 int main(void)
 {
-char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(2, message, 43);
+char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+char *endl = "\n";
+write(2, message, 64);
+write(2, endl, 10);
 return (1);
 }
