9d4f8bcbb02923e01e12954916935cbf2b37bcbd
Lennox Mountain <lennoxmountain67@gmail.com>
2023-02-19 23:17:53 -0800

    uppercase and lower case alphabet letters

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 /**
- *main - prints all letters from a to z in lower case
+ *main - prints all lower case and uppercase alphabet letters
  *Return: 0 on success
  */
 int main(void)
 {
 char letter = 'a';
 while (letter <= 'z')
+{
+putchar(letter);
+letter = letter + 1;
+}
+/*upper case letters*/
+letter = 'A';
+while (letter <= 'Z')
 {
 putchar(letter);
 letter = letter + 1;
