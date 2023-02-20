0df23ae052a3b61976ae47a89cc70ef12f182520
120lennox <89992666+120lennox@users.noreply.github.com>
2023-02-21 00:55:36 +0200

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,6 +9,6 @@ int main(void)
 char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 char *endl = "\n";
 write(2, message, 64);
-write(2, endl, 4);
+write(2, endl, 2);
 return (1);
 }
