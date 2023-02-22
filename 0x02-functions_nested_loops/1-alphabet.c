cd202e06ac51296c1b27621ff4a82f6991df9f47
Lennox Mountain <lennoxmountain67@gmail.com>
2023-02-22 07:32:35 -0800

    alphabet letters

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *print_alphabet - prints alphabet
+ *Return: 0 on success
+ */
+void print_alphabet(void)
+{
+int letter;
+for (letter = 99; letter <= 122; i++)
+{
+_putchar(letter);
+}
+_putchar('\n');
+}
