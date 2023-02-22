90a88cb46fcfe617c489072253a278ae45e1529d
Lennox Mountain <lennoxmountain67@gmail.com>
2023-02-22 07:51:19 -0800

    alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - prints alphabet 10 times
+ *Return: 0 on success
+ */
+void print_alphabet_x10(void)
+{
+int times = 0;
+while (times <= 10)
+{
+char letter = 'a';
+while (letter <= 'z')
+{
+_putchar(letter);
+letter++;
+}
+_putchar('\n');
+times++;
+}
+return (0);
+}
