46f451f5fb9affb6b51fdac1047b84bea541955a
Lennox Mountain <lennoxmountain67@gmail.com>
2023-02-24 15:58:04 -0800

    is int

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *_isdigit - checks if char is num
+ *@c: contains all char
+ *Return: 1 if char otherwise 0
+ */
+int _isdigit(int c)
+{
+if (c >= 48 && c <= 57)
+{
+return (1);
+}
+else
+return (0);
+}
