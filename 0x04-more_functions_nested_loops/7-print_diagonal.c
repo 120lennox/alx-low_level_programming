ca5bdc0d2bbd1a352ee3ac9ea902121493dd08a0
Lennox Mountain <lennoxmountain67@gmail.com>
2023-02-24 17:27:01 -0800

    diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *print_diagonal - prints diagona lines
+ *@n: maximum times the line should be printed
+ */
+void print_diagonal(int n)
+{
+if (n > 0)
+{
+int i, j;
+for (i = 0; i < n; i++)
+{
+for (j = 0; j < i; j++)
+{
+_putchar(' ');
+}
+_putchar('\\');
+_putchar('\n');
+}
+}
+_putchar('\n');
+}
