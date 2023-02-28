00003dbd9a80dae7b2da39a47d96a3439ab95efa
Lennox Mountain <lennoxmountain67@gmail.com>
2023-02-28 03:29:39 -0800

    reverse print

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse followed by a new line
+ * @s: pointer to the string to be printed in reverse
+ */
+void print_rev(char *s)
+{
+int length = 0;
+while (*s != 0)
+{
+length++;
+s++;
+}
+s--;
+while (length > 0)
+{
+_putchar(*s);
+s--;
+length--;
+}
+_putchar('\n');
+}
