42e7fc5201506ff1a6865ad9af80d5feac1f7954
Lennox Mountain <lennoxmountain67@gmail.com>
2023-02-28 01:56:09 -0800

    reset value of pointer n

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ *reset_to_98 - resets the value of pointer *n to 98
+ * @n: holds the address of n
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This directory has all tasks about pointers, arrays and strings
