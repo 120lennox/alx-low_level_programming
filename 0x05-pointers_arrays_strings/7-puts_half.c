87111bb52606da6e5f7e1f1381e9532510c5a083
Lennox Mountain <lennoxmountain67@gmail.com>
2023-02-28 12:30:12 -0800

    prints half of string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,15 +1,17 @@
 #include <string.h>
 #include "main.h"
 /**
+ *puts_half - prints half of string
+ *@str: address that contain string value
  */
 void puts_half(char *str)
 {
-  int string_len = strlen(str);
-  int begin = string_len % 2 == 0 ? string_len / 2 : (string_len - 1) /2 + 1;
-  int i;
-  for (i = begin; i < string_len; i++)
-    {
-      _putchar(str[i]);
-    }
-  _putchar('\n');
+int string_len = strlen(str);
+int begin = string_len % 2 == 0 ? string_len / 2 : (string_len - 1) / 2 + 1;
+int i;
+for (i = begin; i < string_len; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
 }
