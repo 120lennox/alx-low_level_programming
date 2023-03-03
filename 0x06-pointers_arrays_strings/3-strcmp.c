c755ad711add908861d38129482a15215799a712
Lennox Mountain <lennoxmountain67@gmail.com>
2023-03-03 12:32:00 -0800

    compare str

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,13 @@
+#include <string.h>
+#include "main.h"
+#include <stdio.h>
+/**
+ *_strcmp - compares two strings
+ *@s1: pointer holds value for string 1
+ *@s2: pointer holds value for string 2
+ *Return: returns a strcmp function on success
+ */
+int _strcmp(char *s1, char *s2)
+{
+return (strcmp(s1, s2));
+}
