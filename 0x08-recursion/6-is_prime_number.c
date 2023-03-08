4bb94e624cbb1273eb6269c4a74820605fa168b2
Lennox Mountain <lennoxmountain67@gmail.com>
2023-03-08 14:20:59 -0800

    prime nums

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * is_prime_number_helper - helps tp get prime numbers
+ * @n: num1
+ * @i: num 2
+ * Return: prime
+ */
+
+/**
+ *is_prime_number - gets prime num
+ * @n: num
+ * Return: recursive func
+ */
+int is_prime_number_helper(int n, int i)
+{
+if (n <= 1)
+{
+return (0);
+}
+else if (i > n / 2)
+{
+return (1);
+}
+else if (n % i == 0)
+{
+return (0);
+}
+else
+{
+return (is_prime_number_helper(n, i + 1));
+}
+}
+int is_prime_number(int n)
+{
+return (is_prime_number_helper(n, 2));
+}
