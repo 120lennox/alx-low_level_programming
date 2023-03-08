e40690a6b3d14dded1943d6a3aae92294e002b03
Lennox Mountain <lennoxmountain67@gmail.com>
2023-03-08 13:12:06 -0800

    reverse string print

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _puts_recursion - prints a reverse string
+ * _print_rev_recursion - prints a reverse string
  * @s: pointer to a string
  */
 void _print_rev_recursion(char *s)
