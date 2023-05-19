eea432b47e471191d3cadc75e5fd469c6a17cf2e
Lennox Mountain <lennoxmountain67@gmail.com>
2023-05-19 04:55:03 -0700

    C is Fun

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+/**
+ * main: prints text
+ * Return: 0 on success
+ */
+int main(void)
+{
+    const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+    char *endline = "\n"; 
+    write(2, message, 58);
+    write(2, endline, 2);
+    return 1;
+}
