f0e32e56a5c4c88e01ec8cd8167bf07b28f1fa41
Lennox Mountain <lennoxmountain67@gmail.com>
2023-05-25 14:59:00 -0700

    Debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,11 @@
+#include "stdio.h"
+#include "main.h"
+/**
+ * main - prints num status
+ * Return: 0 on success
+*/
+int main(void)
+{
+	positive_or_negative(98);
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+Debugging in C. ALX-SE debugging tasks
